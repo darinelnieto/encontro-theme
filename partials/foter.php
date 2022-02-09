0b43a69dcb3c695edfe9f722049d6c4a11abf290
Darinel <darinelnietodiaz2021@gmail.com>
2022-02-09 16:56:38 -0500

    Agregando logos en el footer

--- partials/foter.php
+++ partials/foter.php
@@ -9,6 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly.
 }
 $info_contacto = get_field('contacto', 'option');
+$logos = get_field('logos_footer', 'option');
 ?>
 <section class="foter-partial-3b2d84">
     <div class="content-gris">
@@ -20,6 +21,21 @@ $info_contacto = get_field('contacto', 'option');
                         <br>
                         <?php echo $info_contacto['ciudad_pais'] ?>
                     </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="content-logos-footer">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-12 col-md-4 text-center">
+                    <img src="<?= $logos['logo_izquierda']['url'] ?>" alt="<?= $logos['logo_izquierda']['title'] ?>" class="izquierda" width="100%" height="100%">
+                </div>
+                <div class="col-12 col-md-4 text-center">
+                    <img src="<?= $logos['logo_centro']['url'] ?>" alt="<?= $logos['logo_centro']['title'] ?>" class="centro" width="100%" height="100%">
+                </div>
+                <div class="col-12 col-md-4 text-center">
+                    <img src="<?= $logos['logo_derecha']['url'] ?>" alt="<?= $logos['logo_derecha']['title'] ?>" class="derecha" width="100%" height="100%">
                 </div>
             </div>
         </div>
