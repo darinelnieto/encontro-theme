a836dfc7c6b2a6b97fc348fe506a05d989440d70
Darinel <darinelnietodiaz2021@gmail.com>
2022-02-11 08:44:57 -0500

    Cambios footer

--- partials/foter.php
+++ partials/foter.php
@@ -28,13 +28,13 @@ $logos = get_field('logos_footer', 'option');
     <div class="content-logos-footer">
         <div class="container">
             <div class="row align-items-center">
-                <div class="col-12 col-md-4 text-center">
+                <div class="col-4 text-center">
                     <img src="<?= $logos['logo_izquierda']['url'] ?>" alt="<?= $logos['logo_izquierda']['title'] ?>" class="izquierda" width="100%" height="100%">
                 </div>
-                <div class="col-12 col-md-4 text-center">
+                <div class="col-4 text-center">
                     <img src="<?= $logos['logo_centro']['url'] ?>" alt="<?= $logos['logo_centro']['title'] ?>" class="centro" width="100%" height="100%">
                 </div>
-                <div class="col-12 col-md-4 text-center">
+                <div class="col-4 text-center">
                     <img src="<?= $logos['logo_derecha']['url'] ?>" alt="<?= $logos['logo_derecha']['title'] ?>" class="derecha" width="100%" height="100%">
                 </div>
             </div>
